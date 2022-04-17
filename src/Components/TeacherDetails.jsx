c68627b5e644d02b2bf0cbc677b1af786d429b5f
Dinesh Sharma <93374756+dinoweblog@users.noreply.github.com>
2022-04-18 04:25:56 +0530

    update

--- src/Components/TeacherDetails.jsx
+++ src/Components/TeacherDetails.jsx
@@ -24,36 +24,27 @@ const MainDiv = styled.div`
   .btn {
     height: 38px;
     border: none;
-    background-color: tomato;
-    color: white;
+    background-color: transparent;
+    border: 1px solid #b9b9b9;
+    border-radius: 5px;
+    color: black;
     :hover {
-      opacity: 0.9;
+      border-color: green;
     }
   }
-
+  .searchBtn {
+    margin-left: -25px;
+  }
+  input {
+    width: 250px;
+  }
   .card {
     display: grid;
     grid-template-columns: 30% 30% 30%;
     justify-content: space-between;
   }
 `;
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-`;
 
-const Button = styled.button`
-  padding: 4px;
-  border-radius: 4px;
-  border: 1px dashed green;
-  color: black;
-  background-color: white;
-  font-size: 15px;
-  cursor: pointer;
-`;
 const Table = styled.div`
   font-family: arial, sans-serif;
   border-collapse: collapse;
@@ -71,14 +62,17 @@ export const TeacherDetails = () => {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(3);
+  const [count, setCount] = useState(0);
 
   const navigate = useNavigate();
   const [sort, setSort] = useState(true);
   const [Gender, setGender] = useState("male");
-  const [teachersData, setTeachersData] = useState([]);
+  const [check, setCheck] = useState("true");
+  let [teachersData, setTeachersData] = useState([]);
   const [teacherName, setTeacherName] = useState("");
+  let filterData = [];
 
-  let { teachers } = useSelector((state) => state.teachers);
+  const { teachers } = useSelector((state) => state.teachers);
 
   const dispatch = useDispatch();
   const fetchData = () => {
@@ -86,8 +80,13 @@ export const TeacherDetails = () => {
   };
 
   useEffect(() => {
+    setTeachersData([...teachers]);
     fetchData();
   }, []);
+
+  useEffect(() => {
+    reRender();
+  }, [count]);
 
   //   const getFilterData = () => {
   //     setTeachersData([...teachers]);
@@ -96,32 +95,53 @@ export const TeacherDetails = () => {
   const searchData = () => {
     fetch(
       `https://school-inventory-server.herokuapp.com/teachers/search/${teacherName}`
-    ).then((res) => res.json());
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setTeachersData([...res]);
+      })
+      .catch((error) => {
+        setTeachersData([...teachers]);
+      });
   };
-
-  console.log("teachersData", teachersData);
-  console.log("teachers", teachers);
 
   const filterByGender = () => {
     Gender === "male" ? setGender("female") : setGender("male");
 
-    teachers = teachers.filter(function (e) {
-      return Gender === e.gender.toLowerCase();
-    });
+    setTeachersData([
+      ...teachers.filter((e) => Gender == e.gender.toLowerCase()),
+    ]);
   };
 
   const sortByAge = () => {
-    // Gender === "male" ? setGender("female") : setGender("male");
+    check === "true" ? setCheck("false") : setCheck("true");
 
-    teachers = teachers.sort(function (a, b) {
-      return b.age - a.age;
-    });
+    if (check === "true") {
+      teachersData = teachersData.sort(function (a, b) {
+        return a.age - b.age;
+      });
+    } else {
+      teachersData = teachersData.sort(function (a, b) {
+        return b.age - a.age;
+      });
+    }
   };
+  let currentPosts;
+  let indexOfFirstPost;
+  let indexOfLastPost;
+  let paginate;
+  const reRender = () => {
+    indexOfLastPost = currentPage * postsPerPage;
+    indexOfFirstPost = indexOfLastPost - postsPerPage;
+    currentPosts = teachersData.slice(indexOfFirstPost, indexOfLastPost);
+    paginate = (pageNumber) => setCurrentPage(pageNumber);
+  };
+  indexOfLastPost = currentPage * postsPerPage;
+  indexOfFirstPost = indexOfLastPost - postsPerPage;
+  currentPosts = teachersData.slice(indexOfFirstPost, indexOfLastPost);
+  paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = teachers.slice(indexOfFirstPost, indexOfLastPost);
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  console.log(filterData);
 
   return (
     <MainDiv>
@@ -131,6 +151,7 @@ export const TeacherDetails = () => {
           className="btn"
           onClick={() => {
             filterByGender();
+            setCount(1);
           }}
         >
           filter by gender
@@ -139,17 +160,28 @@ export const TeacherDetails = () => {
           className="btn"
           onClick={() => {
             sortByAge();
+            setCount(2);
           }}
         >
           sort by age
         </button>
         <input
           type="text"
+          placeholder="Search"
           value={teacherName}
           onChange={(e) => {
             setTeacherName(e.target.value);
           }}
         />
+        <button
+          className="btn searchBtn"
+          onClick={() => {
+            searchData();
+            setCount(3);
+          }}
+        >
+          Click
+        </button>
       </div>
       <div className="card">
         {currentPosts.map((e) => (
